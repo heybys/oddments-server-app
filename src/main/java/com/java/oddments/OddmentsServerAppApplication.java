ce011a016b07817551098ed878f9b7a770574205
heybys <byn0174@gmail.com>
2024-08-26 01:06:01 +0900

    feat: init project

---
+++ src/main/java/com/java/oddments/OddmentsServerAppApplication.java
@@ -0,0 +1,13 @@
+package com.java.oddments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OddmentsServerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OddmentsServerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/oddments/OddmentsServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.oddments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OddmentsServerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
