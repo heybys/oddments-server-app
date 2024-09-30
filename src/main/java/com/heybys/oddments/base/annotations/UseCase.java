a83739246846d89d4906cfe63ca452d82c1e8c84
es-ysbyun <es-ysbyun@smilegate.net>
2024-09-30 13:20:01 +0900

    fix: update use case annotation

--- src/main/java/com/heybys/oddments/base/annotations/UseCase.java
+++ src/main/java/com/heybys/oddments/base/annotations/UseCase.java
@@ -1,9 +1,17 @@
 package com.heybys.oddments.base.annotations;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 @Service
 @Validated
 public @interface UseCase {
